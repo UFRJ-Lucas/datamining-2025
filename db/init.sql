4a8b407983e67de45952fa26663132969a83188a
Lucas Araujo Carvalho <lucasaraujo180701@hotmail.com>
2025-06-09 18:03:36 -0300

    Cria função para criar linha de trajetória do ônibus

--- db/init.sql
+++ db/init.sql
@@ -45,6 +45,52 @@ BEGIN
 END
 $BODY$;
 
+-- Function: estimate_bus_trajectory
+-- Purpose: Estimates the trajectory of a bus line based on its order and line number.
+-- Parameters:
+--   p_table_name: Name of the table containing GPS data.
+--   p_bus_line: The bus line to analyze.
+--   p_tolerance: Tolerance for simplifying the trajectory (default is 0.01).
+-- Returns:
+--   A table with the following columns:
+--     - ordem: The bus id.
+--     - trajectory: A simplified line geometry representing the bus trajectory.
+CREATE OR REPLACE FUNCTION public.estimate_bus_trajectory(
+	p_table_name text,
+	p_bus_line text,
+    tolerance double precision DEFAULT 0.01
+)
+RETURNS TABLE(
+    ordem text,
+    trajectory geometry
+)
+LANGUAGE 'plpgsql'
+COST 100
+VOLATILE PARALLEL UNSAFE 
+ROWS 1000
+AS $BODY$
+BEGIN
+	-- 1. Get a temporary table of the bus points for the specified line
+    EXECUTE format('
+        CREATE TEMP TABLE IF NOT EXISTS temp_bus_points AS
+        SELECT ordem, geom, to_timestamp(datahoraservidor/1000) AS ts
+        FROM %I
+        WHERE linha = %L',
+        p_table_name, p_bus_line
+    );
+
+	-- 2. Return a simplified line geometry of the bus trajectory
+    RETURN QUERY EXECUTE format('
+        SELECT ordem, ST_Simplify(ST_MakeLine(geom ORDER BY ts), %L) AS trajectory
+        FROM temp_bus_points
+        GROUP BY ordem',
+        tolerance);
+    
+    -- Clean up temporary table
+    DROP TABLE IF EXISTS temp_bus_points;
+END
+$BODY$;
+
 -- Function: calculate_bus_dwell_statistics
 -- Purpose: Calculates bus dwell statistics for a given bus line within a grid.
 -- Parameters:
